97d4f9d9827e75a805c7481c8b6ce7c57e80032a
NATSURAZOR <ulyan.super2405@gmail.com>
2022-02-09 01:50:36 +0100

    connect routers and partition files to folders

--- src/App.js
+++ src/App.js
@@ -1,75 +1,22 @@
 import React, { useEffect, useState } from "react";
-import Movie from "./Movie";
+import Nav from "./Nav/Nav";
+import Home from "./Search/FindMovie";
+import About from "./AboutTheFilm/AboutTheMovie";
+import Favorite from "./Favorites/Favorites";
 import "./App.css";
-
-const test = (movies) => {
-  try {
-    return (
-      <div className="movies">
-        {movies.map((movie) => (
-          <Movie
-            key={movie.Title}
-            title={movie.Title}
-            year={movie.Year}
-            image={movie.Poster}
-          />
-        ))}
-      </div>
-    );
-  } catch (err) {
-    return (
-      <div>
-        <h1>Not Founded</h1>
-      </div>
-    );
-  }
-};
+import "./Nav/Nav.css";
+import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
 
 const App = () => {
-  const API_KEY = "dff14555";
-
-  const [movies, setMovies] = useState([]);
-  const [search, setSearch] = useState("");
-  const [query, setQuery] = useState("");
-
-  useEffect(() => {
-    getMovies();
-  }, [query]);
-
-  const getMovies = async () => {
-    const response = await fetch(
-      `http://www.omdbapi.com/?s=${query}&apikey=${API_KEY}`
-    );
-    const data = await response.json();
-    setMovies(data.Search);
-    console.log(data.Search);
-  };
-
-  const updateSearch = (e) => {
-    setSearch(e.target.value);
-  };
-
-  const getSearch = (e) => {
-    e.preventDefault();
-    setQuery(search);
-    setSearch("");
-  };
-
   return (
-    <div className="App">
-      <form onSubmit={getSearch} className="search-form">
-        <input
-          type="text"
-          className="search-bar"
-          value={search}
-          onChange={updateSearch}
-        />
-        <button className="search-button" type="submit">
-          &#128269;
-        </button>
-      </form>
-      {test(movies)}
-    </div>
+    <Router>
+      <div className="App">
+        <Nav />
+        <Route path="/" element={Home} />
+        <Route path="/movie/:id" element={About} />
+        <Route path="/my-favorites" element={Favorite} />
+      </div>
+    </Router>
   );
 };
 
---
+++ src/Nav/Nav.js
@@ -0,0 +1,14 @@
+import React from "react";
+
+const Nav = () => {
+  return (
+    <nav>
+      <ul>
+        <li>Search Movie</li>
+        <li>Favorites</li>
+      </ul>
+    </nav>
+  );
+};
+
+export default Nav;
